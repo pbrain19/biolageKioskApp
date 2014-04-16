9968dcdfbd485d3845265d6bf49ea7b6effb9d02
paul <paul@paulilaptop>
2014-04-16 13:28:51 -0400

    initial commit

---
+++ js/app.js
@@ -0,0 +1,17 @@
+'use strict';
+
+
+// Declare app level module which depends on filters, and services
+angular.module('myApp', [
+  'ngRoute',
+  'myApp.filters',
+  'myApp.services',
+  'myApp.directives',
+  'myApp.controllers',
+  'ngAnimate'
+]).
+config(['$routeProvider', function($routeProvider) {
+  $routeProvider.when('/home', {templateUrl: 'partials/home.html', controller: 'homeCTRL'});
+  $routeProvider.when('/sweepstakes', {templateUrl: 'partials/sweepstakes.html', controller: 'sweepstakesCTRL'});
+  $routeProvider.otherwise({redirectTo: '/home'});
+}]);
