66194c04813d654bdaa20d9b4e7b56cf83de432e
pbrain19 <pbrain19@gmail.com>
2014-04-18 16:35:47 -0400

    updated sweepstakes with progress bar

--- js/controllers.js
+++ js/controllers.js
@@ -8,8 +8,6 @@ angular.module('myApp.controllers', [])
             $scope.imagesets = [
                 ['/set1/biolaggirl.png', '/set1/touch.png', '/set1/to.png', '/set1/enter.png', '/set1/and.png', '/set1/win.png'],
                 ['/set2/biolagebottles.png', '/set2/find.png', '/set2/our.png', '/set2/designers.png', '/set2/picks.png', '/set2/see.png']
-
-
             ];
             $scope.currentIndex = 0;
             $scope.sweepstakes = function() {
@@ -26,29 +24,26 @@ angular.module('myApp.controllers', [])
             $scope.radio = {model: undefined};
             $scope.radiosss = {model: undefined};
             $scope.radios = {model: undefined};
-            $scope.radiosss = {model: undefined};
-            $scope.radios = {model: undefined};
-
             $scope.radioss = {model: undefined};
             $scope.answerone = function(choice) {
                 $scope.a1 = choice;
             };
-
             $scope.answertwo = function(choice) {
                 $scope.a2 = choice;
 
             };
             $scope.answerthree = function(choice) {
                 $scope.a3 = choice;
-            }
-
+            };
             $scope.answerfour = function(choice) {
                 $scope.userIntent = choice;
             };
             $scope.retakequiz = function() {
-
+                $scope.radio = {model: undefined};
+                $scope.radiosss = {model: undefined}; 
+                $scope.radios = {model: undefined};
                 $scope.sweepstakesPivot = '';
-            }
+            };
             $http.get('js/products.json').success(function(data) {
                 $scope.q1 = data.q1;
                 $scope.q2 = data.q2;
@@ -56,13 +51,13 @@ angular.module('myApp.controllers', [])
                 $scope.q4 = data.q4;
                 $scope.a4 = data.a4;
                 $scope.designers = data.designers;
-            })
+            });
             $scope.prevq = function() {
                 $scope.sweepstakesPivot--;
-            }
+            };
             $scope.nextq = function() {
                 $scope.sweepstakesPivot++;
-            }
+            };
 
 
         });
